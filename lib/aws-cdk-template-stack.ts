dff01181dbabef7a98800e6549cb41748a7fd8b2
admiswalker <admiswalker@gmail.com>
2023-05-19 23:38:19 +0900

    add nat instance to auto power on and off

--- lib/aws-cdk-template-stack.ts
+++ lib/aws-cdk-template-stack.ts
@@ -15,7 +15,7 @@ export class AwsCdkTemplateStack extends cdk.Stack {
     super(scope, id, props);
 
     // VPC
-    const nat_instance = ec2.NatProvider.instance({
+    const nat_provider = ec2.NatProvider.instance({
       instanceType: new InstanceType('t3a.nano'),
       machineImage: new NatInstanceImage(),
       defaultAllowedTraffic: ec2.NatTrafficDirection.OUTBOUND_ONLY,
@@ -23,7 +23,7 @@ export class AwsCdkTemplateStack extends cdk.Stack {
     const vpc = new ec2.Vpc(this, props.prj_name+'-'+this.constructor.name+'-vpc_for_ec2_and_ssm', {
       cidr: '10.0.0.0/16',
       natGateways: 1,
-      natGatewayProvider: nat_instance,
+      natGatewayProvider: nat_provider,
       subnetConfiguration: [
         {
           name: 'Public',
@@ -37,6 +37,7 @@ export class AwsCdkTemplateStack extends cdk.Stack {
         },
       ],
     });
+    const nat_instance = vpc.node.findChild('PublicSubnet1').node.findChild('NatInstance') as ec2.Instance;
 
     // SSM
     const ssm_iam_role = new iam.Role(this, props.prj_name+'-'+this.constructor.name+'-iam_role_for_ssm', {
@@ -92,7 +93,7 @@ export class AwsCdkTemplateStack extends cdk.Stack {
       securityGroup: ec2_sg,
     });
 
-    nat_instance.connections.allowFrom(ec2_sg, ec2.Port.allTraffic());
+    nat_provider.connections.allowFrom(ec2_sg, ec2.Port.allTraffic());
 
     // EC2 Auto Shotdown and Startup
     const ec2_schedule_iam_role = new iam.Role(this, props.prj_name+'-'+this.constructor.name+'-iam_role_for_ec2_schedule', {
@@ -106,13 +107,13 @@ export class AwsCdkTemplateStack extends cdk.Stack {
         flexibleTimeWindow: {
             mode: "OFF"
         },
-        scheduleExpression: "cron(10 22 ? * MON-FRI *)", // Mins Hours Day Mon Week Year // Start every 22:10 (JST) // ref: https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html
+        scheduleExpression: "cron(00 7 ? * MON-FRI *)", // Mins Hours Day Mon Week Year // Start every 7:00 (JST) // ref: https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html
         scheduleExpressionTimezone: 'Asia/Tokyo',
         description: 'Event that start EC2 instance(s)',
         target: {
           arn: 'arn:aws:scheduler:::aws-sdk:ec2:startInstances',
           roleArn: ec2_schedule_iam_role.roleArn,
-          input: JSON.stringify({ InstanceIds:[ec2_instance.instanceId] }),
+          input: JSON.stringify({ InstanceIds:[nat_instance.instanceId, ec2_instance.instanceId] }),
         },
     });
     new cdk.aws_scheduler.CfnSchedule(this,"ec2-stop-scheduler", {
@@ -120,13 +121,13 @@ export class AwsCdkTemplateStack extends cdk.Stack {
       flexibleTimeWindow: {
           mode: "OFF"
       },
-      scheduleExpression: "cron(15 22 ? * MON-FRI *)", // Mins Hours Day Mon Week Year // Stop every 22:15 (JST) // ref: https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html
+      scheduleExpression: "cron(00 22 ? * MON-FRI *)", // Mins Hours Day Mon Week Year // Stop every 22:00 (JST) // ref: https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html
       scheduleExpressionTimezone: 'Asia/Tokyo',
       description: 'Event that stop EC2 instance(s)',
       target: {
           arn: 'arn:aws:scheduler:::aws-sdk:ec2:stopInstances',
           roleArn: ec2_schedule_iam_role.roleArn,
-          input: JSON.stringify({ InstanceIds:[ec2_instance.instanceId] }),
+          input: JSON.stringify({ InstanceIds:[nat_instance.instanceId, ec2_instance.instanceId] }),
       },
     });
 
